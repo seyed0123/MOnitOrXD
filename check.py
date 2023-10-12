f67af643e8434a85301075183c3add7e6f187378
seyed0123 <seyed123ali123@gmail.com>
2023-10-12 20:13:36 +0330

    adding gpu and battery

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 import psutil
-
+import GPUtil
 
 # Function to check CPU usage
 def check_cpu_usage(threshold=50):
@@ -21,12 +21,17 @@ def check_memory_usage(threshold=80):
 
 
 # Function to check disk space
-def check_disk_space(path='/', threshold=75):
-    disk_usage = psutil.disk_usage(path).percent
-    if disk_usage > threshold:
-        print(f"Low disk space detected: {disk_usage}%")
-    else:
-        print(f"disk space: {disk_usage}%")
+def check_disk_space(threshold=90):
+    disks = psutil.disk_partitions(all=False)
+    for disk in disks:
+        if disk.opts == 'cdrom':
+            continue
+        path = disk.device
+        disk_usage = psutil.disk_usage(path)
+        disk_type = disk.fstype
+        print(
+            f"Disk name:{path[0:2]},disk free:{disk_usage.free / 1024 ** 3}  GB ,Disk usage: {disk_usage.percent}% , "
+            f"Disk FS type{disk_type}")
 
 
 # Function to check network traffic
@@ -37,17 +42,33 @@ def check_network_traffic(threshold=100 * 1024 * 1024):
     else:
         print(f"network traffic: {network_traffic:.2f} MB")
 
-def check_cpu_usage(threshold=50):
-    cpu_usage = psutil.cpu_percent(interval=1)
 
-    if cpu_usage > threshold:
-        print(f"High CPU usage detected: {cpu_usage}%")
+def check_gpu_usage(threshold=50):
+    gpu_usage = psutil.virtual_memory()
+
+    if gpu_usage > threshold:
+        print(f"High gpu usage detected: {gpu_usage}%")
     else:
-        print(f"CPU usage : {cpu_usage}%")
+        print(f"gpu usage : {gpu_usage}%")
+
+
+def check_battery():
+    battery = psutil.sensors_battery()
+    print(f"Battery charge:{battery.percent}% , charging:{battery.power_plugged} ")
+
+
+def get_gpu_temperature():
+    gpus = GPUtil.getGPUs()
+    gpu = gpus[0]
+    print(f"GPU model :{gpu.name}, memory Free:{gpu.memoryFree/1024} GB , memory usage:{(gpu.memoryUsed/gpu.memoryTotal)*100}% , GPU temperature: {gpu.temperature}")
+
 # Function to run health checks
 def run_health_checks():
+
     print("Monitoring the system...")
     check_cpu_usage()
     check_memory_usage()
     check_disk_space()
+    get_gpu_temperature()
     check_network_traffic()
+    check_battery()
