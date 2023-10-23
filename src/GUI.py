ed014b22453d5309c383e4d94c70c33a3e2ed251
seyed0123 <seyed123ali123@gmail.com>
2023-10-23 20:29:47 +0330

    adding readme to the project

--- src/GUI.py
+++ src/GUI.py
@@ -9,7 +9,7 @@ def start():
 
     # Set up the display
     screen_width = 800
-    screen_height = 600
+    screen_height = 400
     system_info = check.run_health_checks()
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("System Info")
